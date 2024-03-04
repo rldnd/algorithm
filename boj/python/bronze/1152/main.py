78be68557e48c9a73848a382c1fb24faea3aed0b
rldnd <gi981226@gmail.com>
2024-03-04 14:08:03 +0900

    feat(python): boj 1152, 2439

---
+++ boj/python/bronze/1152/main.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/1152
+
+text = list(filter(lambda x: x != "", input().split(" ")))
+print(len(text))
---
+++ boj/python/bronze/2439/main.py
@@ -0,0 +1,3 @@
+lines = int(input())
+for i in range(1, lines + 1):
+  print(" " * (lines - i) + "*" * i)
