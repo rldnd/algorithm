4d116647f7d5ea85869a5eb4ffb37cf0751b10a2
rldnd <gi981226@gmail.com>
2024-03-11 12:14:12 +0900

    feat(python):  boj 1181

--- boj/python/silver/1181/main.py
+++ boj/python/silver/1181/main.py
@@ -1,18 +1,10 @@
-def bubble_sort_with_dictionary_order(lst: list):
-    length = len(lst) - 1
-    for i in range(length - 1):
-        for j in range(length - i):
-            if len(lst[j]) > len(lst[j+1]):
-                lst[j], lst[j+1] = lst[j+1], lst[j]
-            if len(lst[j]) == len(lst[j+1]) and lst[j] > lst[j+1]:
-                lst[j], lst[j+1] = lst[j+1], lst[j]
-                
-    return lst
-    
 N = int(input())
 words = []
 for _ in range(N):
     words.append(str(input()))
 
+
 remove_duplicated_list = list(set(words))
-short_order_list = bubble_sort_with_dictionary_order(words)
+remove_duplicated_list.sort(key = lambda x: (len(x), x))
+for word in remove_duplicated_list:
+    print(word)
