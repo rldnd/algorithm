5eb7f7a23a05d3c89858f877e41bcb7905288261
rldnd <gi981226@gmail.com>
2024-03-18 11:41:39 +0900

    feat(python): boj 1966

--- boj/python/silver/1966/main.py
+++ boj/python/silver/1966/main.py
@@ -1,32 +1,28 @@
-# TODO
-
 import sys
 readline = sys.stdin.readline
-
-def checker(arr: list, order: int, count = 0):
-    _count = count
-    _arr = arr
-    _order = order
-    max_number = max(arr)
-    first_number = _arr[0]
     
-    if arr[0] == max_number:
-        _count += 1
-        if order == 0:
-            print(_count)
-            return
-        del _arr[0]
-        _order = len(_arr) - 1
-    else:
-        _arr.insert(len(_arr), first_number)
-        if order == 0:
-            _order = len(_arr) - 1
+def get_queue_output():
+    _, order = map(int, readline().split())
+    docs = list(map(int, readline().split()))
+    out_count = 0
+    max_value = max(docs)
+    
+    while order != -1 and len(docs) > 0:
+        if docs[0] == max_value:
+            docs.pop(0)
+            out_count += 1
+            order -= 1
+            if len(docs) > 0:
+                max_value = max(docs) 
+        elif docs[0] != max_value and order == 0:
+            docs.append(docs.pop(0))
+            order = len(docs) - 1
         else:
-            _order -= 1
-    checker(_arr, _order, _count)
+            docs.append(docs.pop(0))
+            order -= 1
+            
+    print(out_count)
 
-test_case_count = int(readline().rstrip())
-for _ in range(test_case_count):
-    _, order = map(int, readline().rstrip().split())
-    number_list = list(map(int, readline().rstrip().split()))
-    checker(number_list, order)
+case_count = int(readline().rstrip())
+for _ in range(case_count):
+    get_queue_output()
