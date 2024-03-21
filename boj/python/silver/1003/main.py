aa7931a59b045e0d3cc927a3aa1ac01a1ef989f6
rldnd <gi981226@gmail.com>
2024-03-21 22:11:23 +0900

    feat(python): boj 1003

---
+++ boj/python/silver/1003/main.py
@@ -0,0 +1,22 @@
+# MEMO: dynamic programming
+
+zero_count = [0] * 41
+one_count = [0] * 41
+
+zero_count[0] = 1
+one_count[1] = 1
+
+def fibonacci(n):
+    for i in range(2, n + 1):
+        zero_count[i] = zero_count[i - 2] + zero_count[i - 1]
+        one_count[i] = one_count[i - 2] + one_count[i - 1]
+
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+
+    fibonacci(n)
+
+    print(zero_count[n], one_count[n])
