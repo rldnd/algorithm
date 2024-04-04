8e0abffc466e83fdb35e9e8270af2d7e17ae8315
rldnd <gi981226@gmail.com>
2024-04-04 10:58:34 +0900

    feat(python): boj 9375

--- boj/python/silver/9375/main.py
+++ boj/python/silver/9375/main.py
@@ -1,39 +1,14 @@
-# TODO: WIP
 import sys
-
-from itertools import combinations
-
 readline = sys.stdin.readline
-
-def get_combination_multiple(arr, length):
-    count = 0
-    combs = list(map(list, combinations(arr, length)))
-    for lst in combs:
-        mul = 1
-        for item in lst:
-            mul *= item
-        count += mul
-    return count
-    
-def get_count(clothes_count):
-    types_clothes = {}
-    clothes_count_with_types = []
-    count = 0
-    for _ in range(clothes_count):
-        [_, types] = readline().rstrip().split()
-        types_clothes[types] = types_clothes.get(types, 0) + 1
-    for i in types_clothes.values():
-        clothes_count_with_types.append(i)
-
-    if len(clothes_count_with_types) == 1:
-        return clothes_count_with_types[0]
-    
-    for i in range(1, len(clothes_count_with_types) + 1):
-        count += get_combination_multiple(clothes_count_with_types, i)
-
-    return count
 
 test_cases = int(readline())
 for _ in range(test_cases):
+    result = 1
     clothes_count = int(readline())
-    print(get_count(clothes_count))
+    types_count_map = {}
+    for _ in range(clothes_count):
+        types = readline().rstrip().split()[1]
+        types_count_map[types] = types_count_map.get(types, 0) + 1
+    for value in types_count_map.values():
+        result *= (value + 1)
+    print(result - 1)
